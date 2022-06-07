bdb2b91f08b62da858da880f5be7c77c0d63cc4a
DayvidMS <dayvid.ms@hotmail.com>
2022-06-07 19:54:09 -0300

    Feature - criação do navbar e func de logout.

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -1,15 +1,32 @@
 <template>
-    <div>
-        teste 2
+    <div class="navbar">
+        <h3 class="logo"><i class="fa fa-star"></i>Logo Marca</h3>
+        <a href="" @click.prevent="logout" class="logout"> Sair   <i class="fa fa-sign-out-alt"></i></a>
     </div>
 </template>
 
 <script>
 export default {
-    name: "NavbarComponent"
+    name: "NavbarComponent",
+    methods: {
+        logout(){
+            this.$router.push({name: 'login'})
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.navbar{
+    .logo {
+    color: #fff;
+    }
+    .logout{
+        text-decoration: none;
+        color: #fff;
+    }
+
+}
+
 
 </style>
