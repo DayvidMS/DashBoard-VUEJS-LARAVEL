da4b9a53f61a47dceaa454137f8d329eb4f9a991
DayvidMS <dayvid.ms@hotmail.com>
2022-06-08 23:25:55 -0300

    Feature - criação componente sidebar

--- frontend/src/components/SidebarComponent.vue
+++ frontend/src/components/SidebarComponent.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        teste
+    <div class="sidebar">
+       <div class="top">
+           <div class="avatar">
+                <span><i class="fa fa-user icon"></i></span>
+           </div>
+           <div class="info">
+               <p class="name">nome</p>
+               <p class="status">online</p>
+           </div>
+       </div>
+       <div class="menu">
+           <ul>
+               <li></li>
+               <li></li>
+               <li></li>
+               <li></li>
+               <li></li>
+           </ul>
+        
+       </div>
     </div>
 </template>
 
@@ -11,5 +29,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    .sidebar{
+        .top{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            padding: 25px 5px;
+            align-items: center;
+            margin-top: 60px;
+
+            .avatar{
+                margin-right: 10px ;
+
+                span{
+                    display: block;
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                    background-color: #CCC;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 25px ;
+                    color: #666666;
+                }
+            }
+
+            .info{
+
+                p{
+                    margin: 0px;
+                    color: #FFF;
+                }
+                .name{
+                    
+                }   
+                .status{
+                    font-size: 10px;
+                }   
+            }
+        }
+    }
 
 </style>
